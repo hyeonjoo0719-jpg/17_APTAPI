ce010684596cebd5e1ad7db7d3dff94c3ca7120c
hyeonjoo0719 <hyeonjoo0719@gmail.com>
2026-07-15 14:30:51 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from 17-aptapi!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ scrapper.py
@@ -0,0 +1,47 @@
+import requests
+import xml.etree.ElementTree as ET
+
+import os
+from dotenv import load_dotenv
+
+# .env 파일 로드
+load_dotenv()
+SERVICE_KEY=os.getenv("SERVICE_KEY")
+
+
+print(SERVICE_KEY)
+
+def fetch_apt_trade(LAWD_CD,DEAL_YMD):
+    # LAWD_CD="27260"
+    # DEAL_YMD="202606"
+    serviceKey=SERVICE_KEY
+
+    URL="https://apis.data.go.kr/1613000/RTMSDataSvcAptTrade/getRTMSDataSvcAptTrade"
+
+    params={
+        "serviceKey":serviceKey,
+        "LAWD_CD":LAWD_CD,
+        "DEAL_YMD":DEAL_YMD
+    }
+
+
+    response=requests.get(URL,params=params)
+    #print(rseponse.content)
+    root=ET.fromstring(response.content)
+
+    items=root.findall("./body/items/item")
+    print(items)
+    print(len(items))
+
+    results=[]
+    for item in items:
+        row={}
+        for child in item:
+            row[child.tag]=child.text
+        results.append(row)
+        
+    return results
+
+
+
+
